9a209bdadd88853cf4cc9c8cb5ff9ecde6bc2b29
Breno Teixeira <brenotx@gmail.com>
2018-08-11 15:51:29 -0300

    IsUnique final solution

--- src/ArraysAndStrings/IsUnique/Solution.java
+++ src/ArraysAndStrings/IsUnique/Solution.java
@@ -9,18 +9,28 @@ import java.util.List;
 
 public class Solution {
 
+    /**
+     * Returns true if the giving string has only unique characters, otherwise return false.
+     *
+     * Complexity:
+     *      Time: O(n)
+     *      Space: 0(1)
+     *
+     * @param s
+     * @return boolean
+     */
     static boolean isUnique(String s) {
         if (s == null) {
             return false;
         }
 
-        List<String> chars = new ArrayList<>();
+        // We are considering only the ASCII 128 normal characters.
+        // Use 256 if you want to consider the extended ASCII characters.
+        boolean[] char_set = new boolean[128];
 
         for (int i = 0; i < s.length(); i++) {
-            if (chars.contains(s.substring(i, i + 1))) {
-                return false;
-            }
-            chars.add(s.substring(i, i + 1));
+            if (char_set[s.charAt(i)]) return false;
+            char_set[s.charAt(i)] = true;
         }
 
         return true;
